50be55ee21391d02fef2f255a11d0e9043d79788
Georgi Christov <ga.christov@gmail.com>
2026-04-13 09:58:34 +0200

    add PR action checks

---
+++ tools/scripts/secrets.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+# Exports required CI environment secrets to local secrets so that the project can use them.
+# Should be invoked from the root of the project as all paths are relative.
+
+# network credentials (the > is intentional to append to a file)
+#echo GUARDIAN_API_KEY="$GUARDIAN_API_KEY" >> multiplatform/common/network/secrets.properties
+#echo GUARDIAN_API_URL="$GUARDIAN_API_URL" >> multiplatform/common/network/secrets.properties
+
+# Firebase credentials
+echo "$GOOGLE_SERVICES_DEV_JSON" >> mobile/src/debug/google-services.json
+echo "$GOOGLE_SERVICES_PROD_JSON" >> mobile/src/release/google-services.json
+
+# Keystores
+echo "$KEYSTORE_DEBUG_BASE64" | base64 --decode >> deploy/debug.keystore
+echo "$KEYSTORE_RELEASE_BASE64" | base64 --decode >> deploy/release_key.jks
