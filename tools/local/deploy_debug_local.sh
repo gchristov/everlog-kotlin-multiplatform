d07937f2066497728b3557628ca3985a3e9fe73a
Georgi Christov <ga.christov@gmail.com>
2026-04-19 16:03:24 +0200

    remove local docker deployment

--- tools/local/deploy_debug_local.sh
+++
@@ -1,6 +0,0 @@
-# Builds and deploys the project to Firebase locally with Docker
-set -e
-echo "🛠 Build project" && ./gradlew assembleDebug
-echo "🧹 Clean up old Docker resources" && (docker image prune -af)
-echo "🏗 Build Docker image" && docker build -t everlog-debug -f mobile/src/debug/Dockerfile .
-echo "🚀 Run Docker image" && docker run --rm -v $(pwd):/app everlog-debug
