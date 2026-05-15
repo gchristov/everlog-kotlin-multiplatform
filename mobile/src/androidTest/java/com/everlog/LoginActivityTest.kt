00515da7266de54bef4853610512b782b8ee94e7
Georgi Christov <ga.christov@gmail.com>
2026-05-15 16:04:28 +0200

    fix tests

--- mobile/src/androidTest/java/com/everlog/LoginActivityTest.kt
+++ mobile/src/androidTest/java/com/everlog/LoginActivityTest.kt
@@ -1,22 +1,15 @@
 package com.everlog
 
-import android.widget.Button
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.NoMatchingViewException
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
-import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
-import androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withHint
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.everlog.ui.activities.login.LoginActivity
-import org.hamcrest.Matchers.allOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -28,36 +21,23 @@ class LoginActivityTest {
     val activityRule = ActivityScenarioRule(LoginActivity::class.java)
 
     @Test
-    fun clickGetStarted_ShowsRegistrationForm() {
+    fun getStarted_displayed() {
+        onView(withText("Get Started"))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun getStarted_launchesHome() {
         onView(withText("Get Started"))
             .check(matches(isDisplayed()))
 
         onView(withText("Get Started")).perform(click())
 
-        onView(allOf(withText("Sign up"), isDescendantOfA(withId(R.id.toolbar))))
-            .check(matches(isDisplayed()))
-
-        onView(withHint("Name"))
-            .check(matches(isDisplayed()))
-    }
-
-    @Test
-    fun loginWithUser_ShowsHomeScreen() {
-        onView(withText("I have an account")).perform(click())
-
-        onView(allOf(withHint("Email"), isDisplayed()))
-            .perform(typeText(BuildConfig.E2E_TEST_USER_EMAIL), closeSoftKeyboard())
-
-        onView(allOf(withHint("Password"), isDisplayed()))
-            .perform(typeText(BuildConfig.E2E_TEST_USER_PASSWORD), closeSoftKeyboard())
-
-        onView(allOf(withText("Login"), isAssignableFrom(Button::class.java))).perform(click())
-
         waitForView(R.id.tabBar)
 
         onView(withId(R.id.tabBar))
             .check(matches(isDisplayed()))
-            
+
         onView(withId(R.id.addBtn))
             .check(matches(isDisplayed()))
     }
@@ -70,9 +50,9 @@ class LoginActivityTest {
             try {
                 onView(withId(viewId)).check(matches(isDisplayed()))
                 return
-            } catch (e: NoMatchingViewException) {
+            } catch (_: NoMatchingViewException) {
                 Thread.sleep(500)
-            } catch (e: AssertionError) {
+            } catch (_: AssertionError) {
                 Thread.sleep(500)
             }
         }
